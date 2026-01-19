2e3dddab5cd18befa15a6f9847250c0d889b285f
juliocouteau <juliosantss5334@hotmail.com>
2026-01-19 03:31:19 -0300

    feat: adicionando projeto de views assíncronas

---
+++ async_app/views.py
@@ -0,0 +1,12 @@
+import asyncio
+from django.http import HttpResponse
+
+
+async def async_counter_view(request):
+    print("Iniciando contador assíncrono...")
+    
+    
+    await asyncio.sleep(5) 
+    
+    print("Contador finalizado!")
+    return HttpResponse("<h1>Tarefa concluída!</h1><p>A view esperou 5 segundos de forma assíncrona.</p>")
