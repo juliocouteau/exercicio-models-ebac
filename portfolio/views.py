a2483fbffa7e97e461d2fc679f364945da2d8dee
juliocouteau <juliosantss5334@hotmail.com>
2026-01-19 03:06:28 -0300

    feat: implementação dos templates, visual Bootstrap e modelo Post

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_view(request):
+    posts = Post.objects.all().order_by('-id')
+    return render(request, 'portfolio/index.html', {'posts': posts})
--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from portfolio.views import post_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', post_view), 
 ]
