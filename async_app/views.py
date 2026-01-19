beb967211d922afa6e4e79f87b8bcce6ce263ed2
juliocouteau <juliosantss5334@hotmail.com>
2026-01-19 03:35:16 -0300

    ajuste: alterando tempo para habilitar o PR

--- async_app/views.py
+++ async_app/views.py
@@ -6,7 +6,7 @@ async def async_counter_view(request):
     print("Iniciando contador assíncrono...")
     
     
-    await asyncio.sleep(5) 
+    await asyncio.sleep(3) 
     
     print("Contador finalizado!")
     return HttpResponse("<h1>Tarefa concluída!</h1><p>A view esperou 5 segundos de forma assíncrona.</p>")
